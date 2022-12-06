152ea90836067c384156dd7f2aeb83e9c3a0fa1c
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-07 02:37:30 +0300

    fresh start/generated all  migrations

---
+++ db/migrate/20221206232824_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.jsonb :shipping
+      t.jsonb :payment
+      t.string :password_digest
+      t.boolean :isAdmin, default: false
+      
+      t.timestamps
+    end
+  end
+end
