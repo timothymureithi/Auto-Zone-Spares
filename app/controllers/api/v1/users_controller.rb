51da05b413e9c5fcc2b8557e0632673a64a3ead0
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-07 03:34:27 +0300

    POST /user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,4 +18,21 @@ class Api::V1::UsersController < ApplicationController
                 end
         end
 
+        def create
+            @user = User.new(user_params)
+            if @user.save
+                login!
+                render json: { status: :created, user: @user }
+            else
+                render json: { status: 500, errors: @user.errors.full_messages }
+        end
+    end
+
+        def update
+            @user = User.find(params[:id])
+            if @user.update(u)
+        end
+
+        private
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  def login!
+    session[:user_id] = @user.id
+    end
+
+  def logged_in?
+   !!session[:user_id]
+    end
+
+    def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+  
 end
