1d62d85ad28282c767747077c6fc888bd7a6716f
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-07 04:53:56 +0300

    updated seed for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+#users
 user1 = User.create(first_name: 'Timothy', last_name: 'Mureithi', email: 'tim@autozone.com', password: 'asdfg', password_confirmation: 'asdfg', isAdmin: true)
+
+#products
+p1 = Product.create(name: 'Vance & Hines VO2 Insight Air Cleaner For Harley', category: 'Air Filter', image: 'https://www.revzilla.com/product_images/1871/4158/vance_hines_vo2_insight_air_cleaner_for_harley_black_silver_750x750.jpg', description: 'Have a glimpse inside the inner workings of your engine with the fuel and air mixing right before your eyes.', price: 449.99, brand: 'Vance & Hines', countInStock: 8)
+p2 = Product.create(name: 'Dynojet Power Commander V', category: 'Fuel Management', image: 'https://www.revzilla.com/product_images/0170/4246/dynojet_power_commander_v_750x750.jpg', description: 'The Dynojet PCV adjusts your bike’s air/fuel mixture, resulting in more horsepower and torque with a smoother, more responsive throttle.', price: 300, brand: 'DYNOJET', countInStock: 20)
+p3 = Product.create(name: 'Galfer Wave Rotor Front', category: 'Brakes', image: 'https://www.revzilla.com/product_images/0065/8415/galfer_wave_rotors_front_750x750.jpg', description: 'Galfer Wave Brake Rotors are laser cut from a proprietary high-carbon 420 Stainless Steel that has been heat treated and parallel diamond ground to ensure a perfect surface.', price: 320, brand: 'GALFER BRAKING SYSTEMS', countInStock: 15)
