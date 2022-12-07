daef02be2b085d47d00d0ac6a27a9863e9e2ad6e
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-07 05:05:21 +0300

    updated serializer attributes

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,3 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id,:id, :user_id, :cartItems, :shipping, :payment, :itemsPrice, :taxPrice, 
+  :shippingPrice, :totalPrice, :isPaid, :paidAt, :isDelivered, :deliveredAt, :created_at, :updated_at
+  belongs_to :user
 end
