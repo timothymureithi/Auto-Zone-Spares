de7dfa506223eb8f71e07ffc14e5fed6bacfe8e0
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-07 04:31:23 +0300

    destroy session after logout

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -19,7 +19,15 @@ class Api::V1::SessionsController < ApplicationController
     end 
 
     def destroy
-        
+        logout! 
+        render json: { status: 200, logged_out: true}
+    end
+
+    private
+    def session_params
+      params.require(:session).permit(:_json, :email, :password)
+    end
+
 
 
 end
