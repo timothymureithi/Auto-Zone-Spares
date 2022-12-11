942a647f54c7b879ce5901f1b8de8824b797089d
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-11 16:26:05 +0300

    deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,12 +2,5 @@
 # exit on error
 set -o errexit
 
-# Build commands for front end to create the production build
-rm -rf public
-npm install --prefix client && npm run build --prefix client
-cp -a client/build/. public/
-
-# Build commands for back end
 bundle install
-bundle exec rails db:migrate 
-bundle exec rails db:seed # if you have seed data, run this command for the initial deploy only to avoid duplicate records
+bundle exec rake db:migrate
